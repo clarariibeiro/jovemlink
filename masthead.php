1e5fc568b12a658999970005f8d9733e7643e049
clarariibeiro <clara.riibeiro09@gmail.com>
2026-07-29 17:38:18 -0300

    Add files via upload

---
+++ masthead.php
@@ -0,0 +1,10 @@
+<!-- Masthead-->
+        <header class="masthead text-center text-white">
+            <div class="masthead-content container px-4">
+                <div class="container px-4">
+                    <h1 class="masthead-heading mb-0">Seu futuro começa aqui!</h1>
+                    <h2 class="masthead-subheading mb-0"> Conectando você ao mercado de trabalho.</h2>
+                    <a class="btn btn-primary btn-xl rounded-pill mt-5" style="background-color: #0d6efd; border-color: #0d6efd;" href="listarVagas.php">Encontrar Vagas</a>
+                </div>
+            </div>
+        </header>
