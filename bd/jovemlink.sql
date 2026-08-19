ad8c3c94dfb59270e8cc5dfb01318f90947022bf
isabellaln <neress.isabella@gmail.com>
2026-08-19 15:01:16 -0300

    Add files via upload

--- bd/jovemlink.sql
+++ bd/jovemlink.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 05/08/2026 às 22:50
--- Versão do servidor: 8.0.40
+-- Tempo de geração: 19/08/2026 às 19:59
+-- Versão do servidor: 8.4.8
 -- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `jovemlink1`
+-- Banco de dados: `jovemlink`
 --
 
 -- --------------------------------------------------------
@@ -29,16 +29,16 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `candidato` (
   `idCandidato` int NOT NULL,
-  `fotoCandidato` varchar(255) DEFAULT NULL,
-  `nomeCandidato` varchar(250) NOT NULL,
-  `cpfCandidato` varchar(14) NOT NULL,
-  `dataNascimentoCandidato` date DEFAULT NULL,
-  `estadoCandidato` varchar(2) DEFAULT NULL,
-  `cidadeCandidato` varchar(100) DEFAULT NULL,
-  `emailCandidato` varchar(150) NOT NULL,
-  `senhaCandidato` varchar(255) NOT NULL,
-  `nivelCandidato` varchar(20) DEFAULT 'candidato',
-  `dataCadastro` datetime DEFAULT CURRENT_TIMESTAMP
+  `fotoUsuario` varchar(255) DEFAULT NULL,
+  `dataNascimentoUsuario` date DEFAULT NULL,
+  `nomeUsuario` varchar(100) DEFAULT NULL,
+  `cpfUsuario` varchar(14) DEFAULT NULL,
+  `emailUsuario` varchar(100) DEFAULT NULL,
+  `estadoUsuario` varchar(50) DEFAULT NULL,
+  `cidadeUsuario` varchar(50) DEFAULT NULL,
+  `senhaUsuario` varchar(255) DEFAULT NULL,
+  `nivelUsuario` int DEFAULT NULL,
+  `dataCadastro` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -78,6 +78,36 @@ CREATE TABLE `empresa` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idUsuario` int NOT NULL,
+  `fotoUsuario` varchar(255) NOT NULL,
+  `dataNascimentoUsuario` date NOT NULL,
+  `nomeUsuario` varchar(100) NOT NULL,
+  `cpfUsuario` varchar(14) NOT NULL,
+  `emailUsuario` varchar(100) NOT NULL,
+  `estadoUsuario` varchar(50) NOT NULL,
+  `cidadeUsuario` varchar(50) NOT NULL,
+  `senhaUsuario` varchar(32) NOT NULL,
+  `confirmarSenhaUsuario` varchar(32) NOT NULL,
+  `nivelUsuario` varchar(20) DEFAULT 'usuario',
+  `dataCadastro` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuario`, `fotoUsuario`, `dataNascimentoUsuario`, `nomeUsuario`, `cpfUsuario`, `emailUsuario`, `estadoUsuario`, `cidadeUsuario`, `senhaUsuario`, `confirmarSenhaUsuario`, `nivelUsuario`, `dataCadastro`) VALUES
+(1, 'assets/img/elisregina26.jpg', '0200-03-12', 'mel', '34243423432', 'mel@gmail.com', 'PR', 'imbau', '202cb962ac59075b964b07152d234b70', '202cb962ac59075b964b07152d234b70', 'usuario', '2026-08-19 17:34:46'),
+(2, 'assets/img/Captura de tela 2026-08-17 161940.png', '2026-08-22', 'mnel', '31244343424', 'mnel@gmail.com', 'PR', 'imbau', '202cb962ac59075b964b07152d234b70', '202cb962ac59075b964b07152d234b70', 'usuario', '2026-08-19 17:42:07'),
+(3, 'assets/img/Captura de tela 2026-03-17 114552.png', '2026-08-08', 'sara', '32424342343', 'sara@gmail.com', 'PR', 'imbau', '202cb962ac59075b964b07152d234b70', '202cb962ac59075b964b07152d234b70', 'usuario', '2026-08-19 17:55:36');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `vaga`
 --
 
@@ -104,8 +134,8 @@ CREATE TABLE `vaga` (
 --
 ALTER TABLE `candidato`
   ADD PRIMARY KEY (`idCandidato`),
-  ADD UNIQUE KEY `cpfCandidato` (`cpfCandidato`),
-  ADD UNIQUE KEY `emailCandidato` (`emailCandidato`);
+  ADD UNIQUE KEY `cpfCandidato` (`cpfUsuario`),
+  ADD UNIQUE KEY `emailCandidato` (`emailUsuario`);
 
 --
 -- Índices de tabela `candidatura`
@@ -120,6 +150,12 @@ ALTER TABLE `candidatura`
 --
 ALTER TABLE `empresa`
   ADD PRIMARY KEY (`idEmpresa`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idUsuario`);
 
 --
 -- Índices de tabela `vaga`
@@ -149,6 +185,12 @@ ALTER TABLE `candidatura`
 --
 ALTER TABLE `empresa`
   MODIFY `idEmpresa` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUsuario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `vaga`
