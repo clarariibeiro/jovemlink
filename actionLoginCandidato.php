a227a08ba41e86fbc2628e7d04601ffd47f4eeaa
isabellaln <neress.isabella@gmail.com>
2026-08-19 15:10:42 -0300

    Add files via upload

--- actionLoginCandidato.php
+++ actionLoginCandidato.php
@@ -3,21 +3,21 @@
     include "conexaoBD.php"; 
     session_start(); 
 
-    $emailCandidato = mysqli_real_escape_string($conn, $_POST['emailCandidato']); 
-    $senhaCandidato = mysqli_real_escape_string($conn, $_POST['senhaCandidato']);
+    $emailUsuario = mysqli_real_escape_string($conn, $_POST['emailUsuario']); 
+    $senhaUsuario = mysqli_real_escape_string($conn, $_POST['senhaUsuario']);
 
     $buscarLogin = "SELECT * 
                     FROM Candidato 
-                    WHERE emailCandidato = '$emailCandidato' 
-                    AND senhaCandidato = md5('$senhaCandidato')";
+                    WHERE emailUsuario = '$emailUsuario' 
+                    AND senhaUsuario = md5('$senhaUsuario')";
 
     $efetuarLogin = mysqli_query($conn, $buscarLogin); 
 
     if ($registro = mysqli_fetch_assoc($efetuarLogin)) {
         $_SESSION['idCandidato']    = $registro['idCandidato'];
-        $_SESSION['nomeCandidato']  = $registro['nomeCandidato'];
-        $_SESSION['emailCandidato'] = $registro['emailCandidato'];
-        $_SESSION['nivelCandidato'] = $registro['nivelCandidato'];
+        $_SESSION['nomeUsuario']  = $registro['nomeUsuario'];
+        $_SESSION['emailUsuario'] = $registro['emailUsuario'];
+        $_SESSION['nivelUsuario'] = $registro['nivelUsuario'];
         $_SESSION['logado']       = true;
 
         header("Location: index.php");
